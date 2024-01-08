2bc8b3ffb1ad55b67a021bc09dc936ca678dc3e4
Sarwar-Shawon <sshawon95@gmail.com>
2024-01-08 21:56:35 +0000

    wip... config server

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -2,9 +2,9 @@
  * @copyRight by md sarwar hoshen.
  */
 const apiConfig = {
-  SERVER_TEST: 'http://192.168.0.146:3000/gevs',
+  SERVER_TEST: 'http://localhost:3000/gevs',
   // SERVER_LIVE: 'http://192.168.0.146:3000/gevs',
-  SERVER_LIVE: 'https://gevs-t3s2.onrender.com/gevs',
+  SERVER_LIVE: 'http://localhost:3000/gevs',
 };
 
 export default apiConfig;
